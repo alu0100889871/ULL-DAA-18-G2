3a23fdaa46c0e9fc24214c624b9ef7130c466c13
Ernesto Echeverría González <alu0100881622@ull.edu.es>
2018-04-17 12:45:41 +0100

    Point class created

---
+++ src/Point.java
@@ -0,0 +1,28 @@
+
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getDistance(Point p) {
+		return Math.sqrt(Math.pow(Math.abs((getX() - p.getX())), 2) + Math.pow(Math.abs((getY() - p.getY())), 2));
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
